2178527ae965618c229504ad23448c8373f2e754
Leandro Morelato <lmorelato@gmail.com>
2017-02-09 22:49:36 -0200

    Initial Again

---
+++ PerformanceLogger/Helper/LoggerHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PerformanceLogger.Helper
+{
+    public static class LoggerHelper
+    {
+        public static async Task WriteTextAsync(string folderPath, string fileName, string logMessage)
+        {
+            if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+
+            try
+            {
+                using (var sw = new StreamWriter(folderPath + fileName, true))
+                {
+                    var logLine = $"{logMessage}\r\n";
+                    await sw.WriteAsync(logLine);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Not found");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error reading from {0}. Message = {1}", folderPath + fileName, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("In Main catch block. Caught: {0}", ex.Message);
+                Console.WriteLine("Inner Exception is {0}", ex.InnerException);
+            }
+        }
+
+
+    }
+}
